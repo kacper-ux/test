0836b0e93d81156fba0f96b33a2d7b570a080efb
kacper-ux <107538600+kacper-ux@users.noreply.github.com>
2022-07-06 17:39:09 +0200

    Update

--- config.php
+++ config.php
@@ -3,4 +3,6 @@
   define("DB_PASSWORD", "2398yru2n8x73tf837ydzn28zxf8392z");
   define("DB_HOST", "localhost");
 
+
+define("WAS_KEY", "dalkfnr9348hgu3viucg5384iuh34f943gh78f9udx93cn8gt5384cx9m02zw");
 ?>
